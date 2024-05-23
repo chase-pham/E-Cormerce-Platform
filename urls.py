b9bbb360aa1fbcd8f593a35b84795d70fc328664
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-22 19:28:04 -0500

    urls.py

---
+++ urls.py
@@ -0,0 +1,7 @@
+# shop/urls.py
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/review/', views.add_review, name='add_review'),
+    path('profile/', views.profile, name='profile'),
+]
