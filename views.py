f9c1528f616f3144a29521053098476b29491bda
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-23 19:20:55 -0500

    views.py

--- views.py
+++ views.py
@@ -2,6 +2,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserChangeForm
+from django.db.models import Count
+from .models import Product, OrderItem
 
 @login_required
 def profile(request):
@@ -20,3 +22,7 @@ def order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id, user=request.user)
     return render(request, 'shop/order_detail.html', {'order': order})
 
+def recommended_products(user):
+    user_orders = OrderItem.objects.filter(order__user=user).values('product_id')
+    recommended = Product.objects.filter(id__in=user_orders).annotate(order_count=Count('orderitem')).order_by('-order_count')[:5]
+    return recommended
