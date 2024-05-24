1d67b9f2a79ef36f4f8fedf801bf067957faeaee
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-23 19:18:18 -0500

    urls.py update

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/review/', views.add_review, name='add_review'),
     path('profile/', views.profile, name='profile'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
