f10b4a0056bfc52d4b3f92597dc65b8022004d01
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-25 07:41:22 -0500

    URLs.py

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('checkout/', views.checkout, name='checkout'),
     path('apply-discount/', views.apply_discount, name='apply_discount'),
+        path('products/<int:product_id>/upload-image/', views.upload_product_image, name='upload_product_image'),
 ]
